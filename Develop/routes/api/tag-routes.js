23f31795ba650e790d33c272a1892118e7c56ea0
EmilioColds <emiliofriastapia2705@outlook.com>
2024-03-31 15:55:36 -0600

    Finished the get routes for tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,11 +2,29 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag, Category } = require('../../models');
 
 router.get('/', async (req, res) => {
-  
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', async (req, res) => {
-  
+  try {
+    const tagData = await Tag.findByPK(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "There's no tag for that ID."});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
