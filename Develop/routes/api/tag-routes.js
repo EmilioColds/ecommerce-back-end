970579b3676083d1cbe5cf61270fb2bcae5166e3
EmilioColds <emiliofriastapia2705@outlook.com>
2024-03-31 12:33:51 -0600

    Fixed an issue

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,29 +2,11 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag, Category } = require('../../models');
 
 router.get('/', async (req, res) => {
-  try {
-    const prodData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag }],
-    });
-    res.status(200).json(prodData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  
 });
 
 router.get('/:id', async (req, res) => {
-  try {
-    const prodData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag }],
-    });
-    if (!prodData) {
-      res.status(404).json({ message: "There's no ID for that product." });
-      return;
-    }
-    res.status(200).json(prodData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  
 });
 
 router.post('/', async (req, res) => {
